50faef7b3b24679725af049f0720f8cafc06fe9c
luke-dcz <lukedacruz@icloud.com>
2024-10-21 15:45:56 +0100

    add credential auth provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,38 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials"
+import { getUserByEmail } from "./data/users";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: []
+	session: {
+		strategy: 'jwt'
+	},
+	providers: [
+		Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+			authorize: async (credentials) => {
+				if (credentials === null) return null;            
+				    try {
+				        const user = getUserByEmail(
+							typeof credentials.email === 'string' ? credentials.email : ''
+						);
+				        if (user) {
+				            const isMatch = user?.password === credentials.password;
+	
+				            if (isMatch) {
+				                return user;
+				            } else {
+				                throw new Error("Email or Password is not correct");
+				            }
+				        } else {
+				            throw new Error("User not found");
+				        }
+				    } catch (error: any) {
+				        throw new Error(error)
+				    }
+			}
+        }),
+	]
 })
