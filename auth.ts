00b7569698cc352d98b666887357481a3c50dd48
luke-dcz <lukedacruz@icloud.com>
2024-10-29 14:38:53 +0000

    add github provider to auth.ts

--- auth.ts
+++ auth.ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials"
 import { getUserByEmail } from "./data/users";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	session: {
 		strategy: 'jwt'
 	},
 	providers: [
+		GitHub,
 		Credentials({
             credentials: {
                 email: {},
