e513f5f48a6c03d0cfd739925960a173e9d33ab1
Luke Da Cruz <75254839+luke-dcz@users.noreply.github.com>
2024-10-30 16:12:17 +0000

    Add 0auth authentication (#4)
    * add github provider to auth.ts
    
    * create github signin
    
    * add sign in to OAuth page
    
    * add image remote pattern
    
    * add google sign in method

--- app/oauth/page.tsx
+++ app/oauth/page.tsx
@@ -1,3 +1,35 @@
+import { BackButton } from "@/components/back-button";
+import { GithubSignIn } from "@/components/github-signin";
+import { GoogleSignIn } from "@/components/google-signin";
+
 export default function OAuthPage() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-4 p-4 w-full px-8">
+      <div>
+        <BackButton />
+      </div>
+      <h1 className="text-center">OAuth Authentication</h1>
+      <div className="flex flex-col gap-4 mt-8 ">
+        <p>
+          OAuth is a highly reliable authentication option as it relies on third
+          party services. It allows users to sign in/up with their preffered
+          Provider (i.e. Google, Github, etc...).
+        </p>
+        <p>
+          OAuth services spend significant amounts of money, time, and
+          engineering effort to build abuse detection (bot-protection,
+          rate-limiting), password management (password reset, credential
+          stuffing, rotation), data security (encryption/salting, strength
+          validation), and much more. It is likely that your application would
+          benefit from leveraging these battle-tested solutions rather than try
+          to rebuild them from scratch.
+        </p>
+      </div>
+      <h3>Below are various OAuth providers to log in with:</h3>
+      <div className="w-full flex justify-between mt-4 px-16">
+        <GithubSignIn />
+        <GoogleSignIn />
+      </div>
+    </main>
+  );
 }
--- auth.ts
+++ auth.ts
@@ -1,12 +1,16 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials"
 import { getUserByEmail } from "./data/users";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	session: {
 		strategy: 'jwt'
 	},
 	providers: [
+		GitHub,
+		Google,
 		Credentials({
             credentials: {
                 email: {},
